be989ae860fc34c6fdcec91ac71f4d6d943a5075
bennydz <51126502+bennyhdz@users.noreply.github.com>
2022-10-01 17:35:58 -0500

    revert

--- src/lua/functions/core/game/global_functions.cpp
+++ src/lua/functions/core/game/global_functions.cpp
@@ -104,35 +104,6 @@ int GlobalFunctions::luaDoPlayerAddItem(lua_State* L) {
 	return 1;
 }
 
-int GlobalFunctions::luaIsScriptsInterface(lua_State* L)
-{
-	//isScriptsInterface()
-	if (getScriptEnv()->getScriptInterface() == &g_scripts().getScriptInterface()) {	
-		pushBoolean(L, true);
-	} else {
-		reportErrorFunc("EventCallback: can only be called inside (data/scripts/)");
-		pushBoolean(L, false);
-	}
-	return 1;
-}
-
-int GlobalFunctions::luaTablePack(lua_State* L)
-{
-	// table.pack(...)
-	int i;
-	int n = lua_gettop(L);  /* number of elements to pack */
-	lua_createtable(L, n, 1);  /* create result table */
-	lua_insert(L, 1);  /* put it at index 1 */
-	for (i = n; i >= 1; i--)  /* assign elements */
-		lua_rawseti(L, 1, i);
-		if (luaL_callmeta(L, -1, "__index") != 0) {
-			lua_replace(L, -2);
-		}
-	lua_pushinteger(L, n);
-	lua_setfield(L, 1, "n");  /* t.n = number of elements */
-	return 1;  /* return table */
-}
-
 int GlobalFunctions::luaDoSetCreatureLight(lua_State* L) {
 	// doSetCreatureLight(cid, lightLevel, lightColor, time)
 	Creature* creature = getCreature(L, 1);
