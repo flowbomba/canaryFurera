05112d26a5924827c475b660922bee67374e02db
bennydz <51126502+bennyhdz@users.noreply.github.com>
2022-11-19 03:10:15 -0600

    Fixed remove items from stash on create market offer and others things

--- src/creatures/players/player.h
+++ src/creatures/players/player.h
@@ -548,7 +548,7 @@ class Player final : public Creature, public Cylinder
 
 			stashItems[itemId] = amount;
 		}
-		uint16_t getStashItemCount(uint16_t itemId) const {
+		uint32_t getStashItemCount(uint16_t itemId) const {	
 			auto it = stashItems.find(itemId);
 			if (it != stashItems.end()) {
 				return static_cast<uint16_t>(it->second);
@@ -2047,6 +2047,8 @@ class Player final : public Creature, public Cylinder
 		void updateDepotSearchExhausted() {
 			lastDepotSearchInteraction = OTSYS_TIME();
 		}
+
+		std::pair<std::vector<Item*>, std::map<uint16_t, uint32_t>> requestLockerItems(DepotLocker *depotLocker) const;
 
 	private:
 		std::forward_list<Condition*> getMuteConditions() const;
